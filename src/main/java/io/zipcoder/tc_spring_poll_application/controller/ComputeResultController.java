fe11cc7f843d6174bf1558c1642715fb1b019cde
Zach <zach@zipcodes-MacBook-Pro-6.local>
2021-08-17 11:12:16 -0400

    Finished part 4, votes still seeming to apply at random

---
+++ src/main/java/io/zipcoder/tc_spring_poll_application/controller/ComputeResultController.java
@@ -0,0 +1,53 @@
+package io.zipcoder.tc_spring_poll_application.controller;
+
+import dtos.OptionCount;
+import dtos.VoteResult;
+import io.zipcoder.tc_spring_poll_application.domain.Option;
+import io.zipcoder.tc_spring_poll_application.domain.Vote;
+import io.zipcoder.tc_spring_poll_application.repositories.VoteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class ComputeResultController {
+
+    private VoteRepository voteRepository;
+
+    @Autowired
+    public ComputeResultController(VoteRepository voteRepository) {
+        this.voteRepository = voteRepository;
+    }
+
+    @RequestMapping(value = "/computeresult", method = RequestMethod.GET)
+    public ResponseEntity<?> computeResult(@RequestParam Long pollId) {
+        VoteResult voteResult = new VoteResult();
+        Iterable<Vote> allVotes = voteRepository.findVotesByPoll(pollId);
+        OptionCount optionCount;
+        Integer allVotesCount = 0;
+        //Map<OptionCount, Long> voteCounts = new HashMap<OptionCount, Long>();
+        Map<Long, OptionCount> voteCounts = new HashMap<>();
+        for (Vote element : allVotes) {
+            //voteResult.setTotalVotes(voteResult.getTotalVotes() + 1);
+            allVotesCount++;
+            optionCount = voteCounts.get(element.getOption().getId());
+            if (optionCount == null) {
+                optionCount = new OptionCount();
+                optionCount.setOptionId(element.getOption().getId());
+                voteCounts.put(element.getOption().getId(), optionCount);
+            }
+            optionCount.setCount(optionCount.getCount() + 1);
+        }
+        voteResult.setTotalVotes(allVotesCount);
+        voteResult.setResults(voteCounts.values());
+        return new ResponseEntity<VoteResult>(voteResult, HttpStatus.OK);
+    }
+}
+
